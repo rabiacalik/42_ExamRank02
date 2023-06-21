e80e4d8deb63637ac21a0ae9ddb31dfec39d6de4
Rabia Calik <115617420+rabiacalik@users.noreply.github.com>
2023-06-21 13:52:21 +0300

    update flood_fill

--- Level4/flood_fill/flood_fill.c
+++ Level4/flood_fill/flood_fill.c
@@ -1,4 +1,5 @@
 #include "flood_fill.h"
+#include <unistd.h>
 
 void fill(char **tab, t_point uzunluk, t_point mahmut, char sukufe)
 {
@@ -18,3 +19,36 @@ void flood_fill(char **tab, t_point size,t_point begin)
 {
     fill(tab, size, begin, tab[begin.y][begin.x]);
 }
+
+/*
+int main()
+{
+    // Örnek bir karakter tablosu
+    char *tab[] = {
+        "XXXXX",
+        "XOOOX",
+        "XOOOX",
+        "XXXXX"
+    };
+
+    t_point size = {5, 4};      // Tablonun boyutu
+    t_point begin = {2, 1};     // Flood fill'in başlayacağı nokta
+
+    flood_fill(tab, size, begin);
+
+    // Sonuç tabloyu yazdırma
+    for (int i = 0; i < size.y; i++)
+    {
+        write(1, tab[i], size.x);
+        write(1, "\n", 1);
+    }
+
+    return 0;
+}
+
+// Cikti : 
+XXXXX
+XFFFX
+XFFFX
+XXXXX
+*/
